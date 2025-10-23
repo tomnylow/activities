dcb2238779c6787ba43a968b16dd6fefdf64f2eb
vniuser <vni@user>
2025-10-23 14:30:05 +0300

    Add activities A and B

--- app/src/main/java/com/example/activities/ActivityA.kt
+++ app/src/main/java/com/example/activities/ActivityA.kt
@@ -1,20 +1,28 @@
 package com.example.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ActivityA : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_a)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        findViewById<Button>(R.id.buttonOpenActB).setOnClickListener {
+            val intent = Intent(this, ActivityB::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
     }
+
 }

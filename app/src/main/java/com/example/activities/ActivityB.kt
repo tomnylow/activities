cf10f4fbd91f103d30c825cacdf343212c8a84bc
vniuser <vni@user>
2025-10-24 13:29:20 +0300

    Activity B changes color

--- app/src/main/java/com/example/activities/ActivityB.kt
+++ app/src/main/java/com/example/activities/ActivityB.kt
@@ -2,12 +2,19 @@ package com.example.activities
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Layout
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.graphics.toColorInt
 
 class ActivityB : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +31,11 @@ class ActivityB : AppCompatActivity() {
 
             startActivity(intent)
         }
+        handleIntent(intent)
+    }
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
     }
 
     companion object {
@@ -34,5 +46,17 @@ class ActivityB : AppCompatActivity() {
                 putExtra(EXTRA_BACKGROUND_COLOR, colorHex)
             }
         }
+    }
+    private fun handleIntent(intent: Intent) {
+        val colorHex = intent.getStringExtra(ActivityB.EXTRA_BACKGROUND_COLOR) ?: "#FF0000"
+        val backgroundColor = try {
+            colorHex.toColorInt()
+        } catch (e: Exception) {
+            Color.GREEN
+        }
+
+        findViewById<ConstraintLayout>(R.id.main).setBackgroundColor(backgroundColor)
+        findViewById<TextView>(R.id.textViewColor).text = if (!colorHex.isEmpty()) colorHex else "Green"
+        Log.d("ActB", "Handling intent")
     }
 }

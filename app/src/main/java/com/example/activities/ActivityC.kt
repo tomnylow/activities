6191eaec763ad91d51f34b6cf841c8be802367cf
vniuser <vni@user>
2025-11-10 15:55:26 +0300

    finish task B-C

--- app/src/main/java/com/example/activities/ActivityC.kt
+++ app/src/main/java/com/example/activities/ActivityC.kt
@@ -1,6 +1,8 @@
 package com.example.activities
 
+import android.app.ActivityManager
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.WindowManager
@@ -11,10 +13,37 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ActivityC : AppCompatActivity() {
+    override fun onResume() {
+        super.onResume()
+        printActivityStack()
+    }
+
+    fun printActivityStack(tag: String = "ActivityStack") {
+        val activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+
+        Log.d(tag, "=== CURRENT APP TASKS ===")
+
+        activityManager.appTasks.forEachIndexed { taskIndex, appTask ->
+            val taskInfo = appTask.taskInfo
+
+            if (taskInfo.id == -1) return
+
+            Log.d(tag, "AppTask #$taskIndex")
+            Log.d(tag, "\tTask ID: ${taskInfo.id}")
+            Log.d(tag, "\tNumber of Activities: ${taskInfo.numActivities}")
+            Log.d(tag, "\tBase Activity: ${taskInfo.baseActivity?.className}")
+            Log.d(tag, "\tTop Activity: ${taskInfo.topActivity?.className}")
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                Log.d(tag, "\tisRunning: ${taskInfo.isRunning}")
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
         setContentView(R.layout.activity_c)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -25,6 +54,7 @@ class ActivityC : AppCompatActivity() {
             val intent = Intent(this, ActivityA::class.java)
 
             startActivity(intent)
+            finishAffinity()
         }
         Log.d("ActC", "onCreate")
     }
